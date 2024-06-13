22fb7cf2adbb8ff032d00f476cea85c0cdccd200
Hung Luu <luuduchunghmd1619@gmail.com>
2024-06-13 18:32:00 +0700

    fix bug dark mode, done

--- components/LoadingOverlay.tsx
+++ components/LoadingOverlay.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const LoadingOverlay = () => (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-white">
-        <div className="flex flex-col items-center">
-            <div className="h-80 w-80 relative mb-0 mt-[-4rem]">
-                <Image src="/running.gif" alt="Loading" layout="fill" objectFit="cover" />
-            </div>
-            <div className="mt-[-25px] text-3xl font-bold z-10 italic">
-                NOW LOADING
-            </div>
-        </div>
+  <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-white">
+    <div className="flex flex-col items-center">
+      <div className="h-80 w-80 relative mb-0 mt-[-4rem]">
+        <Image
+          src="/running.gif"
+          alt="Loading"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="mt-[-25px] text-3xl font-bold z-[10000] italic">
+        NOW LOADING
+      </div>
     </div>
+  </div>
 );
 
 export default LoadingOverlay;
